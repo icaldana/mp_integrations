570c41b173b58dc7c26e048a28ed84cf0d63fa0d
Ismael Caldana <ismael.caldana@mercadolivre.com>
2023-05-16 18:20:13 -0300

    working on checkout pro

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,51 +1,78 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-const cors = require("cors");
-const mercadopago = require("mercadopago");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+import mercadopago from "mercadopago";
+import { CreatePreferencePayload } from "mercadopago/models/preferences/create-payload.model";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
 mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN,
+  access_token: String(process.env.ACCESS_TOKEN),
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express typescript server running');
+app.get("/", (req: Request, res: Response) => {
+  //res.send('Express typescript server running');
+  res.sendFile(path.join(__dirname + "../../../client/cow_legacy/index.html"));
 });
 
-app.post('/preference', (req: Request, res: Response) => {
-    let preference = {
-        items: [{
-            title: req.body.description,
-            unit_price: Number(req.body.price),
-            quantity: Number(req.body.quantity),
-        }],
-        back_urls: {
-            success: "http://localhost:3000",
-            failure: "http://localhost:3000",
-            pending: "http://localhost:3000"
-        },
-        auto_return: "approved",
-    };
+app.post("/preference", (req: Request, res: Response) => {
+  let preference: CreatePreferencePayload = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity),
+      },
+    ],
+    back_urls: {
+      success: "http://localhost:3000",
+      failure: "http://localhost:3000",
+      pending: "http://localhost:3000",
+    },
+    statement_descriptor: "MP INTEGRATIONS",
+    external_reference: "mp_integrations",
+    auto_return: "approved",
+    binary_mode: false,
+    // purpose: 'wallet_purchase',
+    shipments: {
+      cost: 1000,
+      mode: "not_specified",
+    },
+  };
 
-    mercadopago.preferences.create(preference)
-        .then(function (response: { body: { id: string; }; }) {
-            global.id = response.body.id;
-            res.json({
-                id: response.body.id
-            });
-        }).catch(function (error: any) {
-            console.log(error);
-        });
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response: { body: { id: string } }) {
+      global.id = response.body.id;
+      res.json({
+        preference: response.body,
+      });
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+});
+
+app.post("/process_payment", (req: Request, res: Response) => {
+  console.log(req.body);
+  mercadopago.payment
+    .save(req.body)
+    .then(function (response) {
+      const { status, status_detail, id } = response.body;
+      res.status(response.status).json({ status, status_detail, id });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 
 app.listen(port, () => {
-    console.log(`⚡️ Server is running at localhost:${port}!`);
+  console.log(`⚡️ Server is running at localhost:${port}!`);
 });
